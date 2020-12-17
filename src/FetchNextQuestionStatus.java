8edd8ecc6a8c92a6c0520729f3261ed44df9c0eb
nicktz1408 <nikolaostzavidas@outlook.com>
2020-12-17 16:56:04 +0100

    Filesystem changes

--- src/FetchNextQuestionStatus.java
+++
@@ -1,5 +0,0 @@
-public enum FetchNextQuestionStatus {
-    NEXT_QUESTION,
-    NEXT_ROUND,
-    GAME_FINISHED
-}
--- src/RoundInterface.java
+++
@@ -1,6 +0,0 @@
-public interface RoundInterface {
-    Question getCurrentQuestion();
-    boolean fetchNextQuestion();
-    void answerQuestion(Player player, int answerIndex);
-    String getRoundName();
-}

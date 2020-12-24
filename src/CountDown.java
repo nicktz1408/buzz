6ee5cd43efd2bbd15903a96da067a918752b981e
manolis <eemmanoua@csd.auth.gr>
2020-12-24 19:29:22 +0200

    CountDown timer for the Stop the Clock type of game.

---
+++ src/CountDown.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class CountDown extends JPanel {
+    JLabel label;
+    Timer timer;
+    int second = 5;
+    int millliSecond = 1;
+    private int remainingTime = 0;
+
+    public CountDown() {
+        label = new JLabel("Υπολοιπόμενος χρόνος: "+second);
+        setLayout(new GridBagLayout());
+        add(label);
+        timer = new Timer(1, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                millliSecond -= 1;
+                if (second>0) {
+                    if(millliSecond==0) {
+                        millliSecond = 999;
+                        second--;
+                    }
+                    label.setText("Υπολοιπόμενος χρόνος: " + second + ": " + millliSecond);
+                } else {
+                    ((Timer) (e.getSource())).stop();
+                    label.setText("Ο χρόνος τελείωσε");
+                }
+            }
+        });
+
+        timer.setInitialDelay(0);
+        timer.start();
+    }
+
+    public void buttonClicked(){
+        timer.stop();
+        int remainingTime = second*1000+millliSecond;
+    }
+}

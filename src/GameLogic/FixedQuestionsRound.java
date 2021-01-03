88923a3c92e7a53d05ff099db0c5a758d07ff5d0
nicktz1408 <nikolaostzavidas@outlook.com>
2021-01-03 14:39:37 +0100

    Added JavaDoc to the round/round types and small bug fixed

--- src/GameLogic/FixedQuestionsRound.java
+++ src/GameLogic/FixedQuestionsRound.java
@@ -13,6 +13,12 @@ public abstract class FixedQuestionsRound implements RoundInterface {
         questionsList = new ArrayList<Question>();
     }
 
+    /**
+     * Returns the current question on the queue
+     * @param player the associated player we need to apply the action on
+     * @return the current Question
+     */
+    @Override
     public Question getCurrentQuestion(GamePlayer player) {
         if(questionsList.size() <= currQuestionIndex) {
             // throw new Exception();
@@ -22,12 +28,22 @@ public abstract class FixedQuestionsRound implements RoundInterface {
         return questionsList.get(currQuestionIndex);
     }
 
+    /**
+     * Instructs the system to move to the next question
+     * @param player the associated player we need to apply the action on
+     * @return
+     */
+    @Override
     public boolean fetchNextQuestion(GamePlayer player) {
         currQuestionIndex++;
 
         return currQuestionIndex < questionsList.size();
     }
 
+    /**
+     * Adds a question to our Container (to be used ONLY while building the round)
+     * @param question the question to be added
+     */
     public void addQuestion(Question question) {
         questionsList.add(question);
     }
--- src/GameLogic/StopClockRound.java
+++ src/GameLogic/StopClockRound.java
@@ -1,9 +1,18 @@
 package GameLogic;
 
+/**
+ * Class for the Stop the Clock Round Type (σταμάτησε το χρονόμετρο)
+ */
 public class StopClockRound extends FixedQuestionsRound{
     private final int ROUND_NAME = 2;
     private int milli;
 
+    /**
+     * Answers the current question by a given player
+     * @param player the player that answers the question
+     * @param answerIndex the chosen index of the answer
+     * @param additionalRequestData {int betSize} a single parameter for the amount of the bet by the player
+     */
     @Override
     public void answerQuestion(GamePlayer player, int answerIndex, Object... additionalRequestData) {
         int millisRemaining = (int)additionalRequestData[0];
@@ -16,11 +25,18 @@ public class StopClockRound extends FixedQuestionsRound{
         player.setScore(player.getScore() + scoreToAdd);
     }
 
+    /**
+     * @return the round id
+     */
     @Override
     public int getRoundName() {
         return this.ROUND_NAME;
     }
 
+    /**
+     * Utility to calculate the score bestowed to the given plater
+     * @return the value of the points to be bestowed
+     */
     public double calculateScore(int milli){
         return milli*0.2;
     }

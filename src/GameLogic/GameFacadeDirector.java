e88ede8d41d594e932e2b429e9a954acbd5d3551
nicktz1408 <nikolaostzavidas@outlook.com>
2021-01-04 15:04:46 +0100

    Enhanced the GameFacadeDirector class to support both solo and 2 player games

--- src/GameLogic/GameFacadeDirector.java
+++ src/GameLogic/GameFacadeDirector.java
@@ -7,73 +7,136 @@ import java.util.ArrayList;
  */
 public class GameFacadeDirector {
     /**
-     *
+     * Builds the GameFacade for a single Player
      * @return
      */
-    public GameFacade buildGame(){
+    public GameFacade buildSoloGame() {
         QuestionRepository questionRepository = QuestionRepository.getInstance();
         FixedQuestionsRoundBuilder roundBuilder = new FixedQuestionsRoundBuilder();
         RoundFactory roundFactory = new RoundFactory();
 
-        ArrayList<Question> myQuestions = questionRepository.getRandomQuestionBatch(35);
+        ArrayList<Question> myQuestions = questionRepository.getRandomQuestionBatch(30);
+        int questionIndex = 0;
 
         GameFacadeBuilder gameFacadeBuilder = new GameFacadeBuilder();
 
         return gameFacadeBuilder
                 .addRound(roundBuilder
                         .type(roundFactory.getRightQuestionRound())
-                        .addQuestion(myQuestions.get(0))
-                        .addQuestion(myQuestions.get(1))
-                        .addQuestion(myQuestions.get(2))
-                        .addQuestion(myQuestions.get(3))
-                        .addQuestion(myQuestions.get(4))
-                        .build())
-                /*.addRound(roundBuilder
-                        .type(roundFactory.getStopClockRound())
-                        .addQuestion(myQuestions.get(5))
-                        .addQuestion(myQuestions.get(6))
-                        .addQuestion(myQuestions.get(7))
-                        .addQuestion(myQuestions.get(8))
-                        .addQuestion(myQuestions.get(9))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
                         .build())
                 .addRound(roundBuilder
                         .type(roundFactory.getBettingRound())
-                        .addQuestion(myQuestions.get(10))
-                        .addQuestion(myQuestions.get(11))
-                        .addQuestion(myQuestions.get(12))
-                        .addQuestion(myQuestions.get(13))
-                        .addQuestion(myQuestions.get(14))
-                        .build())*/
-                .addRound(roundFactory.getThermometerRound())
-                /*.addRound(roundBuilder
-                        .type(roundFactory.getRightQuestionAnswerRound())
-                        .addQuestion(myQuestions.get(20))
-                        .addQuestion(myQuestions.get(21))
-                        .addQuestion(myQuestions.get(22))
-                        .addQuestion(myQuestions.get(23))
-                        .addQuestion(myQuestions.get(24))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
                         .build())
                 .addRound(roundBuilder
-                        .type(roundFactory.getRightQuestionAnswerRound())
-                        .addQuestion(myQuestions.get(25))
-                        .addQuestion(myQuestions.get(26))
-                        .addQuestion(myQuestions.get(27))
-                        .addQuestion(myQuestions.get(28))
-                        .addQuestion(myQuestions.get(29))
-                        .build())*/
+                        .type(roundFactory.getStopClockRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getBettingRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getStopClockRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getRightQuestionRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex))
+                        .build())
+                .build();
+    }
+
+    /**
+     * Builds the GameFacade for a 2 Players
+     * @return
+     */
+    public GameFacade buildTwoPlayersGame() {
+        QuestionRepository questionRepository = QuestionRepository.getInstance();
+        FixedQuestionsRoundBuilder roundBuilder = new FixedQuestionsRoundBuilder();
+        RoundFactory roundFactory = new RoundFactory();
+
+        ArrayList<Question> myQuestions = questionRepository.getRandomQuestionBatch(25);
+        int questionIndex = 0;
+
+        GameFacadeBuilder gameFacadeBuilder = new GameFacadeBuilder();
+
+        return gameFacadeBuilder
+                .addRound(roundBuilder
+                        .type(roundFactory.getRightQuestionRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getBettingRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getStopClockRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundFactory.getThermometerRound())
                 .addRound(roundBuilder
                         .type(roundFactory.getQuickAnswerRound())
-                        .addQuestion(myQuestions.get(30))
-                        .addQuestion(myQuestions.get(31))
-                        .addQuestion(myQuestions.get(32))
-                        .addQuestion(myQuestions.get(33))
-                        .addQuestion(myQuestions.get(34))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .build())
+                .addRound(roundBuilder
+                        .type(roundFactory.getRightQuestionRound())
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex++))
+                        .addQuestion(myQuestions.get(questionIndex))
                         .build())
                 .build();
     }
 
 
-    public GameFacade solo(){
+    /**
+     *
+     * @return
+     */
+    public GameFacade buildGame(){
         QuestionRepository questionRepository = QuestionRepository.getInstance();
         FixedQuestionsRoundBuilder roundBuilder = new FixedQuestionsRoundBuilder();
         RoundFactory roundFactory = new RoundFactory();
